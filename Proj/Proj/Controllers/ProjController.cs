83f9fece3c42946e161ef6451d04fce545d7c064
lebogang nchabeleng <u17345180@tuks.co.za>
2019-09-18 20:38:51 +0200

    der

---
+++ Proj/Proj/Controllers/ProjController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proj.Controllers
+{
+    public class ProjController : Controller
+    {
+        // GET: Proj
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
